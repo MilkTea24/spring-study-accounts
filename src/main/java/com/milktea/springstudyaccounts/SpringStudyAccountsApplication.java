97f38e017aa6a4fd6606f43acc2aca104656e4e9
MilkTea24 <kimmanjae15@pusan.ac.kr>
2025-02-24 21:52:16 +0900

    Initial commit

---
+++ src/main/java/com/milktea/springstudyaccounts/SpringStudyAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.milktea.springstudyaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyAccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyAccountsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milktea/springstudyaccounts/SpringStudyAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milktea.springstudyaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyAccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
